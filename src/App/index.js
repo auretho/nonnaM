3cb78976d5429b024614ed14e4af49d96881e94d
auretho <aurelie.thouzeau@gmail.com>
2021-02-07 17:35:14 +0100

    mep structure

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import Nav from '../Nav';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav/index.js
@@ -0,0 +1,10 @@
+import React from'react';
+import './style.scss';
+
+const Nav = () => (
+    <div className="navigation">
+        Voici la nav
+    </div>
+)
+
+export default Nav;
