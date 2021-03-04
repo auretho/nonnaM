0992e3d860281c71fb562b5dd98be912ff39847c
auretho <aurelie.thouzeau@gmail.com>
2021-03-04 18:29:57 +0100

    positionning of the 3 smileys OK on process page

--- src/components/Home/Process/index.js
+++ src/components/Home/Process/index.js
@@ -1,77 +1,48 @@
 import React from 'react';
 import HoverImage from "react-hover-image";
-
-// import arrow from '../../../assets/arrow1.png';
-import smile1 from '../../../assets/smile1.png';
-import smile2 from '../../../assets/smile2.png';
-import scribble from '../../../assets/scribble.png';
-
-
 import './style.scss';
 
 
 const Products = ({location}) => {
-    console.log(location);
-    
-    // A SUPPRIMER SI UTILISATION DE HOVER
-    // const handleClick = () => {
-    //     const text = document.querySelector('.process-text');
-    //     if (text.className === "process-text hidden"){
-    //         text.className = "process-text";
-    //     } else{
-    //         text.className = "process-text hidden";
-    //     }
-    // }
 
-    const handleHover = () => {
-        const arrow = document.querySelector('.process-arrow');
-        const text = document.querySelector('.process-text');
-        
-        text.className = "process-text";
-        arrow.style = "filter: drop-shadow(0 0 5px black)";
+    const handleHover = (evt) => { 
+        evt.target.nextSibling.nextSibling.classList.remove('hidden');
+        evt.target.parentElement.style.filter = "drop-shadow(0 0 5px black)";
     }
 
-    const handleHoverOut = () => {
-        const arrow = document.querySelector('.process-arrow');
-        const text = document.querySelector('.process-text');
-        
-        text.className = "process-text hidden";
-        arrow.style.filter = null;
-    }
-
-    const handleHover2 = () => {
-        const arrow = document.querySelector('.process-arrow2');
-        const text = document.querySelector('.process-text2');
-        
-        text.className = "process-text2";
-        arrow.style = "filter: drop-shadow(0 0 5px black)";
-    }
-
-    const handleHoverOut2 = () => {
-        const arrow = document.querySelector('.process-arrow2');
-        const text = document.querySelector('.process-text2');
-        
-        text.className = "process-text2 hidden";
-        arrow.style.filter = null;
+    const handleHoverOut = (evt) => {
+        evt.target.nextSibling.nextSibling.classList.add('hidden'); 
+        evt.target.parentElement.style.filter = '';
     }
 
     return(
     <div className="process" id="process">
         {
         location.map((element, i) => (
-            <div className="process-img" key={i}>
+            <div 
+                className="process-img" 
+                key={i}
+                style={{
+                    position: `${element.imgBlockPos.pos}`,
+                    top: `${element.imgBlockPos.top}`,
+                    left: `${element.imgBlockPos.left}`,
+            }}>
                 <div 
                     className="process-imageDiv" 
-                    onMouseOver={handleHover} 
-                    onMouseLeave={handleHoverOut} 
-                    style={{
-                        position: `${element.imgBlockPos.pos}`,
-                        top: `${element.imgBlockPos.top}`,
-                        left: `${element.imgBlockPos.left}`,
-                    }}
                 >
                     
-                    <img src={element.arrow} alt="" className="process-arrow" />
+                    <img 
+                        src={element.arrow} 
+                        alt="" 
+                        className="process-arrow" 
+                        onMouseOver={handleHover} 
+                        onMouseLeave={handleHoverOut} 
+                        style={{
+                            position: `${element.arrowPos.pos}`,
+                            top: `${element.arrowPos.top}`,
+                            left: `${element.arrowPos.left}`,
+                        }}
+                    />
                     <HoverImage 
                         className="process-smile" 
                         src={element.img1} 
@@ -79,9 +50,10 @@ const Products = ({location}) => {
                         style={{
                             position: `${element.smileyPos.pos}`,
                             top: `${element.smileyPos.top}`,
+                            left: `${element.smileyPos.left}`,
+
                         }}
                     />
-                
                     <div 
                         className="process-text hidden"
                         style={{
@@ -90,8 +62,8 @@ const Products = ({location}) => {
                             left: `${element.textPos.left}`,
                         }}
                     >
-                        <h1>LES ARTICHAUTS:</h1> 
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, quisquam commodi voluptatum enim repellat vero.</p>
+                        <h1>{element.title}:</h1> 
+                        <p>{element.text}</p>
                     </div>
                 </div>
             </div>
@@ -101,17 +73,3 @@ const Products = ({location}) => {
 )}
 
 export default Products;
-
-
-        {/* IMAGE TWO ============================================= */}
-        // <div className="image img-two">
-        //     <img src={scribble} alt="" className="process-arrow2" />
-        //     <div onMouseOver={handleHover2} onMouseLeave={handleHoverOut2}>
-        //         <HoverImage src={smile1} hoverSrc={smile2} /*onClick={handleClick}*/ className="process-smile2"/>
-        //     </div>
-            
-        //     <div className="process-text2 hidden">
-        //         <h1>L'HUILE D'OLIVE:</h1>
-        //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, quisquam commodi voluptatum enim repellat vero.</p>
-        //     </div>
-        // </div>
--- src/reducers/process.js
+++ src/reducers/process.js
@@ -1,5 +1,7 @@
-import scribble from '../assets/scribble.png';
 import arrow1 from '../assets/arrow1.png';
+import arrow2 from '../assets/scribble.png';
+import arrow3 from '../assets/arrow3.png';
+
 import smile1 from '../assets/smile1.png';
 import smile2 from '../assets/smile2.png';
 
@@ -10,20 +12,84 @@ const initialState = {
             img1: smile1,
             img2: smile2,
             arrow: arrow1,
+            title: 'Les artichauts',
+            text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia officiis amet iusto enim placeat quisquam saepe corrupti hic consectetur repellat.',
             imgBlockPos: {
                 pos: 'relative',
                 top: '55%',
                 left: '60%', 
             },
+            arrowPos: {
+                pos: '',
+                top: '',
+                left: '',
+            },
+
             smileyPos: {
                 pos: 'relative',
-                top: '-150px'
+                top: '-150px',
+                left: '',
             },
             textPos: {
                 pos: 'absolute',
                 top: '-80%',
-                left: '-60%',
-            }
+                left: '110%',
+            },
+        },
+        {
+            img1: smile1,
+            img2: smile2,
+            arrow: arrow2,
+            title: 'L\'huile d\'olive',
+            text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia officiis amet iusto enim placeat quisquam saepe corrupti hic consectetur repellat.',
+            imgBlockPos: {
+                pos: 'relative',
+                top: '55%',
+                left: '10%', 
+            },
+            arrowPos: {
+                pos: 'relative',
+                top: '-70px',
+                left: '-60px',
+            },
+            smileyPos: {
+                pos: 'relative',
+                top: '-230px',
+                left: '-150px',
+            },
+            textPos: {
+                pos: 'absolute',
+                top: '40%',
+                left: '-120%',
+            },
+        },
+        {
+            img1: smile1,
+            img2: smile2,
+            arrow: arrow3,
+            title: 'Le soleil pour les tomates',
+            text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia officiis amet iusto enim placeat quisquam saepe corrupti hic consectetur repellat.',
+            imgBlockPos: {
+                pos: 'relative',
+                top: '20%',
+                left: '5%', 
+            },
+            arrowPos: {
+                pos: '',
+                top: '',
+                left: '',
+            },
+
+            smileyPos: {
+                pos: 'relative',
+                top: '0px',
+                left: '',
+            },
+            textPos: {
+                pos: 'absolute',
+                top: '-30%',
+                left: '120%',
+            },
         },
     ]
 };
