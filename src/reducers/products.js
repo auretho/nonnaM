f9edc0c9a52e2b249a7f3f09b2f52035af01599a
auretho <aurelie.thouzeau@gmail.com>
2021-03-06 12:36:35 +0100

    modif icons products

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,27 +1,35 @@
-import potTomate from '../assets/pot.png';
-import potArtichaut from '../assets/pot2.png';
-import oil from '../assets/oil.png';
+import tomate from '../assets/tomate.png';
+import artichaut from '../assets/artichaut.png';
+import olive from '../assets/olive.png';
+import truffe from '../assets/truffe.png';
+
 
 
 const initialState = {
     img: 
     [
         {
-          image: potTomate,
+          image: tomate,
           shortName: 'tomates',
           name: "Salsa picante",
           description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis porro neque eos esse. Quae, tempore ipsum harum reprehenderit delectus vel." 
         },
         {
-          image: potArtichaut,
+          image: artichaut,
           shortName: 'artichauts',
           name: "Coeurs d'artichauts",
           description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis porro neque eos esse. Quae, tempore ipsum harum reprehenderit delectus vel."  
         },
         {
-            image: oil,
+            image: olive,
             shortName: 'huile',
             name: "Huile d'olive",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis porro neque eos esse. Quae, tempore ipsum harum reprehenderit delectus vel."  
+        },
+        {
+            image: truffe,
+            shortName: 'truffe',
+            name: "Truffe blanche",
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis porro neque eos esse. Quae, tempore ipsum harum reprehenderit delectus vel."  
         },
     ]
