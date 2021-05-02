b4eb83a05ae0121af65b0af73c66d9a928941028
auretho <aurelie.thouzeau@gmail.com>
2021-05-02 17:34:03 +0200

    change location .env

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv'). config({ path: '/Applications/MAMP/htdocs/Perso/nonna/.env' })
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 // const cors = require('cors');
