95e96d6b802df5c0b80f34ae4528857c36d52f6a
auretho <aurelie.thouzeau@gmail.com>
2021-05-03 09:15:31 +0200

    static images modified

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const orderRoute = require('./routes/order');
 const recipeRoute = require('./routes/recipe');
 const legalRoute = require('./routes/legal');
 
-app.use('/images', express.static(path.join(__dirname,'images')));
+app.use(express.static(path.join(__dirname,'images')));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
