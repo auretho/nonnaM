0dc84529bf9c613f9c269f63747cb9e0eb742c33
auretho <aurelie.thouzeau@gmail.com>
2021-07-18 13:21:21 +0200

    modif multer mimetypes

--- backend/mdlw/multer-config.js
+++ backend/mdlw/multer-config.js
@@ -1,15 +1,22 @@
 const multer = require('multer');
 
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+  };
+  
 const storage = multer.diskStorage({
     // dans quel dossier enregistrer le fichier 
     destination: (req, file, cb) => {
-        cb(null, '../build/static/images') // nom du dossier créé pour recevoir les images en 2e argument
+        cb(null, 'images') // nom du dossier créé pour recevoir les images en 2e argument
     },
     // quel nom de fichier utiliser
     filename: (req, file, cb) => {
         // il peut parfois y avoir des espaces autorisés dans les noms de fichier qu'il faut éliminer
         const name = file.originalname.split(' ').join('_'); 
-        cb(null, Date.now() + '_' + name);
+        const extension = MIME_TYPES[file.mimetype];
+        cb(null, Date.now() + '.' + extension);
 
     }
 })
